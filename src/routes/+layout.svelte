0934986a53cad24b029c23edc7970373a1d59e1c
Ronny Garcia <ronny.garcia@llacsaa.com>
2024-01-29 17:34:19 -0500

    v1.1.1 finished

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
-    import "../app.css"
-    import Navbar from "../components/layout/Navbar.svelte";
-    import Footer from "../components/layout/Footer.svelte";
+  import "../app.css";
+  import Navbar from "../components/layout/Navbar.svelte";
+  import Footer from "../components/layout/Footer.svelte";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <Navbar />
 <main class="montserrat">
-    <slot />
+  <slot />
 </main>
 <Footer />
